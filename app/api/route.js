26df1116026d3d7f310d7ea2058d8fc00e4ed535
Fame hyder <famehyder9999@gmail.com>
2025-07-12 11:35:15 +0500

    UDB

--- app/api/route.js
+++ app/api/route.js
@@ -1,8 +1,13 @@
 import { writeFile } from "fs/promises";
-
 await writeFile ("hello.txt", "hello server ");
 
 console.log (process.cwd());
 
 console.log ("this is a test msg");
 
+
+import { readFile } from "fs/promises";
+
+const read = await readFile("hello.txt", "utf-8")
+
+console.log(read)
