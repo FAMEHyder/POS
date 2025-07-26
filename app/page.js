99bdf51e79be4c3720341bc3f6e12b159c4dc475
Fame hyder <famehyder9999@gmail.com>
2025-07-26 18:22:19 +0500

    UDB

--- app/page.js
+++ app/page.js
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <>
     
-    <h1>Hello</h1>
-    <Link href={"/About"}>About</Link> {"  "}
-    <Link href={"/Services/Web_dev"}>web dev</Link>{"  "}
-    <Link href={"/Blogs/BlogID"}>Blogs</Link>
+    
     </>
   );
 }
