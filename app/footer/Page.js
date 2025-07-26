6e74c9a61b3cf1280007d25bc3e6483ea99df091
Fame hyder <famehyder9999@gmail.com>
2025-07-26 18:27:33 +0500

    UDB

--- app/footer/Page.js
+++ app/footer/Page.js
@@ -1,7 +1,15 @@
+import {Grid} from '@emotion/styled';
 function footer() {
 
 
     return (
-        <></>
+        <Grid >
+            <Box></Box>
+            <Box></Box>
+            <Box></Box>
+
+        </Grid>
     )
 }
+
+export default footer;
